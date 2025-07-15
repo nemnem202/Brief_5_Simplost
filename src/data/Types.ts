fd6dd0b8b8096232512ef096596991dc017c6be4
nemnem202 <naimelhabbas@gmail.com>
2025-07-15 16:04:31 +0200

    updated types in master

---
+++ src/data/Types.ts
@@ -0,0 +1,3 @@
+export type Destinations = { label: string; value: string; distanceFromParis: number }[];
+
+export type Standing = { label: string; value: string; pricePerKm: number; perks: string[] }[];
