6943e518eadb6af43182265e5cad78c767c8d596
nemnem202 <naimelhabbas@gmail.com>
2025-07-15 15:50:12 +0200

    corrected types declarations

--- src/Types.ts
+++
@@ -1,3 +0,0 @@
- export type AppData = { 
-    destinations: { label: string; value: string; distanceFromParis: number; }[];
-    standing: { label: string; value: string; pricePerKm: number; perks: string[]; }[]; }
