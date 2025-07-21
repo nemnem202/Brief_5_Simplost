595bdbc8f9ac854c6841149754577d218f65264e
Emilie M. <emilie.marion.simplon@gmail.com>
2025-07-21 13:35:25 +0200

    added dynamic recap + style for payment

--- src/components/PaymentComponent.ts
+++ src/components/PaymentComponent.ts
@@ -1,18 +1,45 @@
 import { Component } from "../lib/component";
 import templateHTML from "../componentsTemplates/paymentComponent.html?raw";
+import type { ClientInformations, FlightInformation } from "../data/Types";
+import { AppManager } from "../appManager";
 
 export class PaymentComponent extends Component {
+  private clientData: ClientInformations | undefined;
+  private travelData: FlightInformation | undefined;
+
   constructor() {
-    super();
-    this.addTemplate();
+    super(templateHTML);
+    this.clientData = AppManager.getInstance().clientInformations;
+    this.travelData = AppManager.getInstance().flightInformation;
+    this.fillTravelInfos();
   }
 
-  private addTemplate(){
-    const wrapper = document.createElement("template");
-    wrapper.innerHTML = templateHTML;
-    const realTemplate = wrapper.content.querySelector("template");
-    if (realTemplate){
-      this.content = realTemplate.content.cloneNode(true) as HTMLElement;
+  private fillTravelInfos(){
+    if(this.content && this.clientData && this.travelData){
+      const divClientInfos: HTMLElement | null = this.content.querySelector("div#clientInfos");
+      if(divClientInfos){
+        this.addTextToHTMLElement(divClientInfos,`${this.clientData.name} ${this.clientData.lastName}`);
+        this.addTextToHTMLElement(divClientInfos,this.clientData.email);
+        this.addTextToHTMLElement(divClientInfos,this.clientData.phoneNumber);
+      }
+
+      const divTravelInfos: HTMLElement | null = this.content.querySelector("div#travelInfos");
+      if(divTravelInfos){
+        this.addTextToHTMLElement(divTravelInfos,`Location: ${this.travelData.originCity} - ${this.travelData.destinationCity}`);
+        this.addTextToHTMLElement(divTravelInfos,`Date: ${this.travelData.date.toDateString()}`);
+        this.addTextToHTMLElement(divTravelInfos,`Class: ${this.travelData.standing} class`);
+      }
+
+      const divPrice: HTMLElement | null = this.content.querySelector("div#price");
+      if(divPrice){
+        this.addTextToHTMLElement(divPrice,`Price: ${this.travelData.price.toString()} €`);
+      }
     }
+  }
+
+  private addTextToHTMLElement(htmlElement: HTMLElement, text: string){
+    const p: HTMLParagraphElement = document.createElement("p");
+    p.innerText = text;
+    htmlElement.appendChild(p);
   }
 }
