bffa285caea211636ef5da20ee285a5154b20123
fraxi0n <85115114+fraxi0n@users.noreply.github.com>
2025-07-23 17:09:33 +0200

    add dynamic information

--- src/components/historyComponent.ts
+++ src/components/historyComponent.ts
@@ -11,22 +11,65 @@ export class HistoryComponent extends Component {
   constructor() {
     super(templateHTML);
 
+    setTimeout(
+      () => {
+        const date = new Date();
+        date.setMonth(8);
+        date.setDate(12);
+
+        AppManager.getInstance().flightInformation = {
+          originCity: "paris",
+          destinationCity: "lisbon",
+          price: 33,
+          standing: "economy",
+          date: date,
+        };
+
+        
+      },
+
+      0
+    );
+
     setTimeout(() => {
       this.flightInformation = AppManager.getInstance().flightInformation;
       this.clientInformation = AppManager.getInstance().clientInformations;
 
       const tic = document.getElementById("travel-info-container");
 
-      if (this.clientInformation && this.clientInformation) {
-        //création du truc
+      if (this.flightInformation) { // && this.clientInformation
+
+        const spanArray : HTMLElement[]= []
+    
+        const changeSpan = (spanID: keyof FlightInformation) => 
+        {
+          const span: HTMLElement | null =document.getElementById(spanID)
+
+          if (span && this.flightInformation)
+          {
+            span.innerText = this.flightInformation[spanID]+''
+            spanArray.push(span)
+          }
+        }
+
+        changeSpan("originCity")
+        changeSpan("destinationCity")
+        changeSpan("price")
+        changeSpan("standing")
+        changeSpan("date")
+
+        //todo  addclass service included
       } else {
         if (tic) {
           tic.innerHTML = "";
         }
       }
       const homeButton = document.getElementById("home-button-container");
-      homeButton?.appendChild(createButton("Go Back to Home Page", "home-button", ()=>AppManager.getInstance().changePage("home")  ))
-
+      homeButton?.appendChild(
+        createButton("Go Back to Home Page", "home-button", () =>
+          AppManager.getInstance().changePage("home")
+        )
+      );
     }, 0);
   }
 }
