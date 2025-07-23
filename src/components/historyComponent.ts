e676d9fbabcc452274f6a5bcb4a531011a3b9065
fraxi0n <85115114+fraxi0n@users.noreply.github.com>
2025-07-23 15:40:34 +0200

    get flight information and return nothing if empty

--- src/components/historyComponent.ts
+++ src/components/historyComponent.ts
@@ -1,16 +1,29 @@
 import { AppManager } from "../appManager";
 import { Component } from "../lib/component";
 import createButton from "../utils/button";
-// import templateHTML from "../componentsTemplates/homeComponent.html?raw";
 import templateHTML from "../componentsTemplates/historyComponent.html?raw";
-
-// const DOM_history = document.querySelector("#historyTemplate")
+import type { ClientInformations, FlightInformation } from "../data/Types";
 
 export class HistoryComponent extends Component {
+  flightInformation: FlightInformation | undefined;
+  clientInformation: ClientInformations | undefined;
+
   constructor() {
     super(templateHTML);
-    
-  
+
+    setTimeout(() => {
+      this.flightInformation = AppManager.getInstance().flightInformation;
+      this.clientInformation = AppManager.getInstance().clientInformations;
+
+      const tic = document.getElementById("travel-info-container");
+
+      if (this.clientInformation && this.clientInformation) {
+        //création du truc
+      } else {
+        if (tic) {
+          tic.innerHTML = "";
+        }
+      }
+    }, 0);
   }
 }
-
